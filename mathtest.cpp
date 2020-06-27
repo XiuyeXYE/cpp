f199975397b745f225d2182ef3a76d37a0699e92
xiuye <xiuye_engineer@outlook.com>
2020-06-27 17:56:24 +0800

    none

--- mathtest.cpp
+++ mathtest.cpp
@@ -1,14 +1,19 @@
 #include"xylib/output"
 #include"xylib/math"
+#include"xylib/time"
 using namespace xiuye;
 int main(){
 
-
+    TimeSpec::begin();
     println(max(5,6,31,22,5));
     println(min(5,6,31,22,5));
 
     println(max(5.0,6.0,31.0,22.0,5.0));
     println(min(5.0,6.0,31.0,22.0,5.0));
 
+    for(int i=0;i<87650;i++)
+        println(i);
+
+    TimeSpec::print_cost_time();
     return 0;
 }
