9c1958947e28ed531bcdecca024ea01b96e3c4c6
xiuye <xiuye_engineer@outlook.com>
2020-07-20 21:39:06 +0800

    lock achievement failed!

--- asm/threadsafe.cpp
+++ asm/threadsafe.cpp
@@ -2,7 +2,8 @@
 
 
 //CAS for thread lock!
-ssize_t cmp_and_swap_q(volatile ssize_t old/*for rax*/,volatile ssize_t *n_ptr/*for memory*/,volatile ssize_t expect/*dest[new value]*/){
+//总之就是 原子 改变 内存的值 也就是 n_ptr 指向的值
+ssize_t cmp_and_swap_q(volatile ssize_t old/*for rax*/,volatile ssize_t *n_ptr/*for memory*/,volatile ssize_t expect/*for dest[new value]*/){
     
     //in the end: *n_ptr <= expect 
 
@@ -11,49 +12,68 @@ ssize_t cmp_and_swap_q(volatile ssize_t old/*for rax*/,volatile ssize_t *n_ptr/*
     ssize_t rbx=0;
 
     asm volatile(
-        "movq %%rax,%0;"//for chekc
-        "movq %%rbx,%1;"//for chekc
-        "lock cmpxchgq %%rbx,(%4)"//lock for atomic : read,modify,write
+        // "movq %%rax,%0;"//for chekc
+        // "movq %%rbx,%1;"//for chekc
+        "lock cmpxchgq %%rbx,%4;"//lock for atomic : read,modify,write
+        "movq %%rax,%0;"
+        "movq %%rbx,%1;"
+        "rep nop;"
         :"=m"(rax),"=m"(rbx)//for chekc
-        :"a"(old),"b"(expect),"r"(n_ptr)
+        :"a"(old),"b"(expect),"m"(*n_ptr)
         :"memory"
 
     );
     // log("rax=",rax,"rbx=",rbx,"*n_ptr",*n_ptr);//for chekc
-    return *n_ptr;
+    return rax/*old rax ->  new rax*/;
+}
+
+
+bool cmp_and_swap_q(volatile ssize_t *n_ptr/*for memory*/,volatile ssize_t expect/*for dest[new value]*/){
+    
+    ssize_t old = *n_ptr;
+
+    if(cmp_and_swap_q(old,n_ptr,expect)==old){
+        return true;
+    }
+    
+    return false;
 }
 
 struct x_mutex{
 
-    // x_mutex():counter(1){
 
-    // }
 
-    x_mutex(int i=1/*default value*/):counter(i),total(i){}
+    x_mutex(int i=1/*default value*/):counter(i),total(i){
+        
+    }
 
 
     ssize_t lock(){ // wait | p()
-        // ssize_t old = counter;
-        // ssize_t expect = ;
-
-        // while(counter == 0){
-        //     cmp_and_swap_q(counter,&counter,counter-1);
-        // }
 
         //没有资源就等待吧! wait() | p()
         //atomic
 
         assert(counter<=total && counter >=0);
 
-        volatile ssize_t old = 0;
-        do{
-            old = counter;
-            log("spin...lock");
-        }while(old <=0 || cmp_and_swap_q(counter,&counter,counter-1)==old);
+
+        //atomic : read -> modify -> write and consistency
+        L1:
+        while(counter==0){
+            asm volatile("rep nop;");
+            // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        if(!cmp_and_swap_q(&counter,0)){
+            goto L1;
+        }
+
+
+
         
+
         assert(counter > -1);
 
-        log("lock:",counter);
+        // log("lock:",counter);
         return counter;
     }
 
@@ -62,35 +82,37 @@ struct x_mutex{
         进入lock通过后,unlock 只需要释放一次就OK了!
     */
     ssize_t unlock(){//signal | V()
-        // while(counter == 0){
-        //     // cmp_and_swap(&counter,1);
-        // }
 
 
         assert(counter > -1);
 
-        //释放资源必须比原来的大,就是释放= 资源+1
-        //如果一直小于和等于原来的资源 ,就一直自旋到 +1 为止
-        volatile ssize_t old = 0;
-        do{
-            old = counter;
-            log("spin...unlock");
-        }while(cmp_and_swap_q(counter,&counter,counter+1)<old);
+
+      
+        cmp_and_swap_q(&counter,1);
+        
 
         assert(counter<=total && counter >=0);
 
-        log("unlcok:",counter);
+        // log("unlcok:",counter);
 
         return counter;
+    }
+
+    ssize_t getCnt(){
+        return counter;
+    }
+
+    ssize_t getTotal(){
+        return total;
     }
 
     private:
         volatile ssize_t counter;
         volatile ssize_t total;
-
-
+        
 
 };
+
 
 struct Counter{
     ssize_t cnt=0;
@@ -113,16 +135,18 @@ int main(){
 
     log("now num=",num,"ret=",ret);
 
+    log(cmp_and_swap_q(&num,num-1));
+
     //thread not safe
 
     x_mutex mtx;
 
-    log("open lock:",mtx.lock());
-    log("close clock:",mtx.unlock());
-    log("open lock:",mtx.lock());
-    log("close clock:",mtx.unlock());
-    log("open lock:",mtx.lock());
-    log("close clock:",mtx.unlock());
+    log("open lock:",mtx.lock(),mtx.getCnt(),mtx.getTotal());
+    log("close clock:",mtx.unlock(),mtx.getCnt(),mtx.getTotal());
+    log("open lock:",mtx.lock(),mtx.getCnt(),mtx.getTotal());
+    log("close clock:",mtx.unlock(),mtx.getCnt(),mtx.getTotal());
+    log("open lock:",mtx.lock(),mtx.getCnt(),mtx.getTotal());
+    log("close clock:",mtx.unlock(),mtx.getCnt(),mtx.getTotal());
 
     Counter counter;
     std::mutex std_mtx;
@@ -148,6 +172,8 @@ int main(){
     TimeSpec::print_cost_ticks(); 
     
     log(std::this_thread::get_id(),"in the end counter=",counter.cnt,counter.st);
+
+    
 
     return 0;
 }
