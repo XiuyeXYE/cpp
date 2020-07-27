d4c5d92f914e1da4bc7f58144bb4823b547ff876
xiuye <xiuye_engineer@outlook.com>
2020-07-27 21:59:13 +0800

    depth and bread search for graph

---
+++ algorithm/graph.cpp
@@ -0,0 +1,422 @@
+#include"../common"
+
+/*https://github.com/XiuyeXYE/cpp*/
+
+using namespace std;
+
+vector<string> split(string& text){
+    std::regex ws_re("\\s+"); // whitespace
+    std::vector<std::string> v(std::sregex_token_iterator(text.begin(), text.end(), ws_re, -1), 
+        std::sregex_token_iterator());
+    return v;
+}   
+
+class graph{
+
+    int v;
+    int e;
+    std::vector<int> **adj;//邻接表 数组 + 线性存储结构
+
+    public:
+        
+        graph(int v):v(v),e(0){
+            init();
+        }
+
+        void init(){
+            // this->v = v;
+            adj = new std::vector<int>*[v];
+            for(int i=0;i<v;i++){
+                adj[i] = new std::vector<int>();
+            }
+        }
+
+        graph(std::istream &in):graph(0){
+            // in.getline();
+            string str_line;
+            getline(in,str_line);
+            // log(str_line);
+            v = stoi(str_line);
+            init();
+            getline(in,str_line);
+            // log(str_line);
+            int e1 = stoi(str_line);
+            
+                       
+            for(int i=0;i<e1;i++){
+                getline(in,str_line);
+                vector<string> vec = split(str_line);
+                int v  = stoi(vec[0]);
+                int w = stoi(vec[1]);
+                add_edge(v,w);
+            }
+
+        }
+
+        int get_v(){
+            return v;
+        }
+
+        int get_e(){
+            return e;
+        }
+
+        void add_edge(int i,int j){
+            // log(i,j);
+            //edge
+            adj[i]->push_back(j);
+            adj[j]->push_back(i);
+            e++;
+        }
+
+        std::vector<int>* operator [](int idx){
+            return adj[idx];
+        }
+
+
+        string to_stong(){
+            //非string 的 类型 必须 to_sting, 否则,不显示!!!
+            string s = std::to_string(v)+" vertices, "+std::to_string(e)+" edges\n";
+            for(int i=0;i<v;i++){
+                s += to_string(i) + ": ";
+                for(auto &w:*(*this)[i]){
+                    s += to_string(w) + " ";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+
+        virtual ~graph(){
+            //没有复制构造函数必须当心释放两次
+            // log("free error");
+            for(int i=0;i<v;i++){
+                delete adj[i];
+                adj[i] = nullptr;
+            }
+            delete []adj;
+            adj = nullptr;
+        }
+
+};
+
+
+int degree(graph& g,int v){
+    int degree = 0;
+    for(auto &w:*g[v]){
+        degree++;
+    }
+    return degree++;
+}
+
+int max_degree(graph &g){
+    int max = 0;
+    for(int v=0;v<g.get_v();v++){
+        if(degree(g,v)>max){
+            max = degree(g,v);
+        }
+        return max;
+    }
+}
+
+double avg_degree(graph& g){//必须用引用,否则函数结束就会释放一次!
+    return 2.0 * g.get_e()/g.get_v();
+}
+
+//自环个数
+int number_of_self_loops(graph &g){
+    int count = 0;
+    // log("loops");
+    for(int v=0;v<g.get_v();v++){
+        // log("v:",v);
+        for(auto &w : *g[v]){
+            // log(typeid(w).name(),typeid(v).name());
+            log(v,w);
+            if(v == w)count++;
+        }
+    }
+    return count/2;
+}
+
+ 
+class depth_first_search{
+
+    bool *marked;
+    int counter;
+
+    void dfs(graph &g,int v){
+        marked[v] = true;
+        counter++;
+        for(auto &w:*g[v]){
+            log(w);
+            if(!marked[w])dfs(g,w);
+        }
+    }
+
+
+    public:
+
+
+        depth_first_search(graph &g,int s):counter(0){
+            marked = new bool[g.get_v()]{};//{} => false list
+            // for(auto i=0;i<g.get_v();i++){
+            //     log(i,marked[i]);
+            // }
+
+            dfs(g,s);
+        }
+
+        bool mark(int w){//与s连通吗 记住是连通
+            return marked[w];
+        }
+
+        int count(){//与s连通的个数 记住是连通
+            return counter;//我觉得这个数据 有问题
+        }
+
+        virtual ~depth_first_search(){
+            delete []marked;
+            marked = nullptr;
+        }
+
+
+
+};
+
+class depth_first_paths{//dfs 的路径是比较长的
+
+    bool *marked ;//
+    int *edge_to;// 树型结构 自底向上
+    int s;//起点;
+
+    void dfs(graph &g,int v){
+        marked[v] = true;
+        for(int &w:*g[v]){
+            if(!marked[w]){
+                edge_to[w] = v;
+                dfs(g,w);
+            }
+        }
+    }
+
+
+
+    public:
+        depth_first_paths(graph &g,int s){
+            marked = new bool[g.get_v()]{};//分配内存的要注意初始化,否则报一些莫名其妙的错误!!!
+            edge_to = new int[g.get_v()]{};
+            this->s = s;
+            dfs(g,s);
+        }
+
+        bool has_path_to(int v){
+            return marked[v];
+        }
+
+        vector<int> path_to(int v){//从s -> v 的路径
+
+            // log("");
+            if(!has_path_to(v)){
+                return vector<int>();
+            }
+            // log("");
+
+
+            
+            vector<int> path;
+            // log("");
+            for(int x=v;x!=s;x=edge_to[x]){
+                // log(x);
+                path.push_back(x);
+            }
+            // log("");
+            path.push_back(s);
+            // log("");
+            return path;
+
+        }
+
+        virtual ~depth_first_paths(){
+            // log("release:");path_to
+            delete []marked;
+            marked = nullptr;
+            delete []edge_to;
+            edge_to = nullptr;
+        }
+
+};
+
+
+
+//广度优先搜索路径
+class bread_first_paths{
+
+    bool *marked;
+    int * edge_to;//存储s -> v的路径中的顶点
+    int s;//起点
+
+    void bfs(graph &g,int s){
+        queue<int> q;
+        marked[s] = true;
+        q.push(s);//尾部添加
+
+        while(!q.empty()){
+            int v = q.front();//不删除元素
+            q.pop();//删除头部元素
+            log("v:",v);
+            for(auto &w:*g[v]){
+                if(!marked[w]){
+                    edge_to[w] = v;
+                    marked[w] = true;
+                    q.push(w);
+                }
+            }
+
+        }
+
+    }
+
+    public:
+        bread_first_paths(graph &g/*rust borrow*/,int s){
+            marked = new bool[g.get_v()]{};//alloc and initialize
+            edge_to = new int[g.get_v()]{};//alloc and initialize
+            this -> s = s;
+            bfs(g,s);
+        }
+
+        bool has_path_to(int v){
+            return marked[v];
+        }
+
+         vector<int> path_to(int v){//从s -> v 的路径
+
+            if(!has_path_to(v)){
+                return vector<int>();
+            }
+            vector<int> path;
+            for(int x=v;x!=s;x=edge_to[x]){
+                path.push_back(x);
+            }
+            path.push_back(s);
+            return path;
+
+        }
+
+        virtual ~bread_first_paths(){
+            // log("release:");path_to
+            delete []marked;
+            marked = nullptr;
+            delete []edge_to;
+            edge_to = nullptr;
+        }
+
+};
+
+
+// #define fmt(arg...) print(123##arg)
+
+// #define fk(...) lk(0,##__VA_ARGS__,65)
+
+// #define lk(_1,_2,_3,n,...) n
+
+// #define t(...) 0,##__VA_ARGS__,11,23
+
+
+struct A{
+    A(){
+        log("constructor");
+    }
+    A(A&)
+    {
+        log("copy constructor");
+    }
+
+};
+
+
+A &f(A &b){
+    log("copy?");
+    return b;
+}
+
+
+
+int main(){
+
+    #ifdef __VA_ARGS__
+        log("has va args");
+    #endif  
+
+    ifstream reader("BST.cpp",ios::in);
+
+    log(reader.is_open());
+    if(reader.is_open()){
+        log("begin:");
+        int c = 0;
+        // log(typeid(reader.get()).name();
+        while(!reader.eof()&&reader.good()){
+            // print((char)reader.get());
+            // reader >> c ;
+            string str;
+            getline(reader,str);
+            println(str);
+        }
+        log("end");
+    }
+
+    reader.close();
+
+    std::string text = "Quick brown fox.";
+    std::regex ws_re("\\s+"); // whitespace
+    std::vector<std::string> v(std::sregex_token_iterator(text.begin(), text.end(), ws_re, -1), 
+        std::sregex_token_iterator());
+    for(auto& s: v)
+        std::cout << s <<"\n";
+
+    A a1;
+    auto c = f(a1);
+    log(typeid(c).name());
+
+    // test for reading 
+    ifstream in("graph.txt");
+    graph g(in);
+
+    log(degree(g,4));
+    log(max_degree(g));
+    log(avg_degree(g));
+    log(number_of_self_loops(g));
+
+    log(g.to_stong());
+
+
+    //深度有限搜索
+    depth_first_search search(g,3);
+
+    log(search.mark(0),search.mark(7),search.count());
+
+    //深度优先的 s->v 的路径是长路径
+    // log("");
+    depth_first_paths path(g,0);
+    // log("");
+    log(path.has_path_to(3),path.path_to(3).size(),path.path_to(3).max_size());
+    log("path 0->3:");
+    for(auto &w:path.path_to(3)){
+        print(w,' ');
+    }
+    println();
+
+    // log("bread");
+    //广度优先 是 短路径!
+    bread_first_paths path2(g,0);
+    // log("");
+    log(path2.has_path_to(3),path2.path_to(3).size(),path2.path_to(3).max_size());
+    log("path2 0->3:");
+    for(auto &w:path2.path_to(3)){
+        print(w,' ');
+    }
+    println();
+
+
+
+    return 0;
+}
